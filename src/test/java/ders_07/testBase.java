00079cea69a1ed1dd1aa96a3edb2ebd87d24d3f0
erhan paker <erhanpaker@gmail.com>
2023-02-01 16:23:01 +0300

    first commtt_1

--- src/test/java/ders_07/testBase.java
+++ src/test/java/ders_07/testBase.java
@@ -1,2 +1,22 @@
-package ders_07;public class testBase {
+package ders_07;
+
+import org.junit.Assert;
+import org.junit.Test;
+import utilits.TestBaseClass;
+
+public class testBase extends TestBaseClass {
+
+    @Test
+
+    public void test01(){
+        // amazon gidelim
+
+        driver.get("https://www.amazon.com");
+        String expectedIcerik = "amazon";
+        String  actualUrl = driver.getCurrentUrl();
+        Assert.assertTrue(actualUrl.contains(expectedIcerik));
+
+
+
+    }
 }
--- src/test/java/utilits/TestBaseClass.java
+++ src/test/java/utilits/TestBaseClass.java
@@ -1,2 +1,29 @@
-package utilits;public class testBaseClass {
+package utilits;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestBaseClass {
+  protected   static WebDriver driver;
+
+    @BeforeClass
+
+    public static void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+
+    public static void teardown(){
+        driver.close();
+
+    }
 }
