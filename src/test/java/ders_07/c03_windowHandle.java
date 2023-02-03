a9bf7a6932f9d00b2d99e7da30ea691076ffe39c
erhan paker <erhanpaker@gmail.com>
2023-02-03 14:23:36 +0300

    first commtt_1.2

--- src/test/java/ders_07/c03_windowHandle.java
+++ src/test/java/ders_07/c03_windowHandle.java
@@ -1,6 +1,9 @@
 package ders_07;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import utilits.TestBaseClass;
 
 public class c03_windowHandle extends TestBaseClass {
@@ -8,6 +11,14 @@ public class c03_windowHandle extends TestBaseClass {
     @Test
 
     public void test01(){
+        driver.get("https://the-internet.herokuapp.com/windows");
+        WebElement yaziElementi = driver.findElement(By.tagName("h3"));
+
+        String expectedYazi = "Opening a new window";
+        String actualYazi = yaziElementi.getText();
+
+        Assert.assertEquals(expectedYazi,actualYazi);
+
 
     }
 
