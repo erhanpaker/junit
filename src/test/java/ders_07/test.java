b166d53126f2edb2c4a16d2e6249f37f82fa7557
erhan <erhanpaker@gmail.com>
2023-03-08 13:39:57 +0300

    testeee

--- src/test/java/ders_07/test.java
+++ src/test/java/ders_07/test.java
@@ -3,8 +3,6 @@ package ders_07;
 public class test {
     public static void main(String[] args) {
 
-        System.out.println("erhan");
-        System.out.println("erhan");
-        System.out.println("hello");
+
     }
 }
